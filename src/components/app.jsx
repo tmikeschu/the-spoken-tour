143e81f72ddb703ada6d851bfd6523606406280a
Mike Schutte <tmikeschutte@gmail.com>
2017-02-20 15:39:46 -0700

    Remove animation from wheels
    Add static styling for about page

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './header.jsx';
 import Nav from './nav.jsx';
+import About from './about.jsx';
 import '../stylesheets/App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="container">
         <Header />
         <Nav />
+        <About />
       </div>
     );
   }
