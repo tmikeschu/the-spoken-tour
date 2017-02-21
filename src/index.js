d641e0ae8a153c5ef913500704de7930478c6790
Mike Schutte <tmikeschutte@gmail.com>
2017-02-21 05:46:40 -0700

    Add preliminary routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+// Libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import { Router, Route, browserHistory } from 'react-router'
+
+// Components
+import App from './components/App';
+import Home from './components/Home';
+import About from './components/About';
+
+// Styles
 import './stylesheets/App.css';
 
 ReactDOM.render(
-  <App />,
+  // <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={Home}/>
+    <Route path="main" component={App} />
+    <Route path="about" component={About} />
+  </Router>,
   document.getElementById('root')
 );
