9be7a20670c7a1994af7f34013f06275c0c13bf4
Mike Schutte <tmikeschutte@gmail.com>
2017-02-21 05:54:53 -0700

    Format landing link

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,9 @@
 // Libraries
 import React, { Component } from 'react';
-import { Router, Route, browserHistory } from 'react-router'
 
 // Components
 import Header from './Header.jsx';
 import Nav from './Nav.jsx';
-import About from './About.jsx';
 
 //  Styles
 import '../stylesheets/App.css';
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,13 +7,11 @@ import '../stylesheets/App.css';
 class Home extends Component {
   render() {
     return (
-      <div className="spacer">
-        <article className="home">
-          <div className="content">
-            <Link to="/main">Hello</Link>
-          </div>
-        </article>
-      </div>
+      <article className="home">
+        <div className="content">
+          <Link to="/main">Find out more...</Link>
+        </div>
+      </article>
     );
   }
 }
