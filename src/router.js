536f26bcce361b0fd17031c4dd4e6d431fb65cc2
Mike Schutte <tmikeschutte@gmail.com>
2017-02-21 07:12:32 -0700

    Resolve build warnings
    Remove extra favicon link

---
+++ src/router.js
@@ -0,0 +1,24 @@
+// Libraries
+import React from 'react';
+import { Router, Route, browserHistory } from 'react-router'
+
+// Components
+import App from './components/App';
+import Home from './components/Home';
+import About from './components/About';
+
+// Styles
+import './stylesheets/App.css';
+
+// Routes
+
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Home}/>
+    <Route component={App}>
+      <Route path="about" component={About} />
+    </Route>
+  </Router>
+);
+
+export default routes;
