23a144a10d0b2480340c51fe69aee973d30cb526
Mike Schutte <tmikeschutte@gmail.com>
2017-02-21 05:58:05 -0700

    Update map and app components

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import Map from 'google-maps-react'
 import '../stylesheets/App.css';
 
-<Map google={this.props.google} zoom={14}>
+ReactDOM.render(
+  <Map google={this.props.google} zoom={14}>
 
-  <Marker onClick={this.onMarkerClick}
-          name={'Current location'} />
+    <Marker onClick={this.onMarkerClick}
+            name={'Current location'} />
 
-  <InfoWindow onClose={this.onInfoWindowClose}>
-      <div>
-        <h1>{this.state.selectedPlace.name}</h1>
-      </div>
-  </InfoWindow>
-</Map>
+    <InfoWindow onClose={this.onInfoWindowClose}>
+        <div>
+          <h1>{this.state.selectedPlace.name}</h1>
+        </div>
+    </InfoWindow>
+  </Map>,
+  document.getElementById('root')
+);
