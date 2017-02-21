17e65d62aab6c9abc3a4b36a7917f3a87fe778dd
Mike Schutte <tmikeschutte@gmail.com>
2017-02-20 17:55:54 -0700

    Add reset scss file
    Remove reset styling from App.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
-import './stylesheets/reset.css';
 import './stylesheets/App.css';
 
 ReactDOM.render(
