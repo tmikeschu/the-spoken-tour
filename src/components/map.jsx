8101b660568e4bcef75be7d91e454eba8fc3198b
Mike Schutte <tmikeschutte@gmail.com>
2017-02-21 06:09:44 -0700

    Refactor to nested routes

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -3,17 +3,22 @@ import ReactDOM from 'react-dom';
 import Map from 'google-maps-react'
 import '../stylesheets/App.css';
 
-ReactDOM.render(
-  <Map google={this.props.google} zoom={14}>
+class TripMap extends Component {
+  render() {
+    return (
+      <Map google={this.props.google} zoom={14}>
 
-    <Marker onClick={this.onMarkerClick}
-            name={'Current location'} />
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
 
-    <InfoWindow onClose={this.onInfoWindowClose}>
-        <div>
-          <h1>{this.state.selectedPlace.name}</h1>
-        </div>
-    </InfoWindow>
-  </Map>,
-  document.getElementById('root')
-);
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+
+export default TripMap;
