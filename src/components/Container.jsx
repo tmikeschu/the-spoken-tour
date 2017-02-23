16cf10b647ffe1288db03b09d426264174fac486
Mike Schutte <tmikeschutte@gmail.com>
2017-02-22 19:48:18 -0700

    Develop map and container comps
    Add jQuery for AJAX

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
+
 import Map from './Map';
 
 export default class Container extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      polylinePoints: [],
+    }
+  }
+
+  componentDidMount() {
+    var self = this;
+    $.ajax({
+      url: "http://spoken-api.herokuapp.com/api/v1/route_pins?api_key="+process.env.REACT_APP_RAILS_KEY,
+      method: "GET",
+    }).done(function(response) {
+      self.setState({
+        polylinePoints: response
+      })
+    }).fail(function(error) {
+      console.error("No");
+    });
+  }
+
+
   render() {
-    const style = { width: '100vw', height: '100vh' };
+    const fullHeightAndWidth = { width: '100vw', height: '100vh' };
+    const mapCenter = {
+      lat: -1.6246706849414423,
+      lng: -85.56376812500002
+    };
+    const mapZoom = 3;
+
+    const markers = [
+      {
+        location: {
+          lat: 39.7392,
+          lng: -104.9903
+        }
+      }
+    ]
 
     return (
-      <div style={style}>
-        <Map />
+      <div style={fullHeightAndWidth}>
+        <Map zoom={mapZoom} center={mapCenter} markers={markers} polylinePoints={this.state.polylinePoints}/>
       </div>
     )
   }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
-import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
+import { GoogleMapLoader, GoogleMap, Marker, Polyline } from 'react-google-maps';
 
 export default class Map extends Component {
   render() {
-    const mapContainer = <div style={{ height: "100%", width: "100%" }} />
+    const mapContainer = <div style={{ height: "90%", width: "75%" }} />
+    const markers = this.props.markers.map((pin, i) => {
+      const marker = {
+        position: {
+          lat: pin.location.lat,
+          lng: pin.location.lng
+        }
+      }
+      return <Marker key={i} {...marker} />
+    })
+
+    const polylinePts = this.props.polylinePoints.map((pin) => {
+      return {
+        lat: parseFloat(pin.location.lat),
+        lng: parseFloat(pin.location.lng)
+      }
+    })
+    const polyOptions = {
+      geodsic: true,
+      strokeColor: "#A00",
+    }
+    const polyline = <Polyline path={polylinePts} options={polyOptions} />
 
     return(
       <GoogleMapLoader
         containerElement={ mapContainer }
         googleMapElement={
           <GoogleMap
-            defaultZoom={14}
-            defaultCenter={{ lat: 39.733984, lng: -105.000536}}
-            options={{ streetViewControl: false, myTypeControl: false }}
-          />
+            defaultZoom={this.props.zoom}
+            defaultCenter={this.props.center}
+            options={{
+              streetViewControl: false,
+              myTypeControl: false,
+            }}>
+            { markers }
+            { polyline }
+          </GoogleMap>
       } />
     );
   }
