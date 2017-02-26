1f7a4903a341d66cb084cd4b00bbfc788b996793
Mike Schutte <tmikeschutte@gmail.com>
2017-02-26 13:16:04 -0700

    Add lifecycle methods

--- src/components/Instagram.jsx
+++ src/components/Instagram.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import '../stylesheets/App.css';
 
 export default class Instagram extends Component {
-  componentDidMount() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      instagramPhotos: [],
+    }
+  }
 
+  componentDidMount() {
+    var self = this;
+    $.ajax({
+      url: "http://spoken-api.herokuapp.com/api/v1/route_pins/instagram_photos?api_key="+process.env.REACT_APP_RAILS_KEY,
+      method: "GET",
+    }).done(function(response) {
+      self.setState({
+        instagramPhotos: response
+      })
+    }).fail(function(error) {
+      console.error("No");
+    });
   }
 
   render() {
