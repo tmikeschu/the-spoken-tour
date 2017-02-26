d13abd6efa53faa12af9e4af5e7827685dff1e2b
Mike Schutte <tmikeschutte@gmail.com>
2017-02-26 12:38:23 -0700

    Add embedded map
    Basic styling for mobile

--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ import { Router, Route, browserHistory } from 'react-router'
 import App from './components/App.jsx';
 import Home from './components/Home.jsx';
 import About from './components/About.jsx';
-import Container from './components/Container.jsx';
+import EmbeddedMap from './components/EmbeddedMap.jsx';
 
 // Styles
 import './stylesheets/App.css';
@@ -17,7 +17,7 @@ const routes = (
     <Route path="/" component={Home}/>
     <Route component={App}>
       <Route path="about" component={About} />
-      <Route path="map" component={Container} />
+      <Route path="map" component={EmbeddedMap} />
     </Route>
   </Router>
 );
