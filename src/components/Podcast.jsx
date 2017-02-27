8231e6bf3cd67659a909e87408114da9e24c4c83
Mike Schutte <tmikeschutte@gmail.com>
2017-02-27 09:00:56 -0700

    Add embedded soundcloud widget

--- src/components/Podcast.jsx
+++ src/components/Podcast.jsx
@@ -6,6 +6,7 @@ export default class Podcast extends Component {
     return (
       <article className="podcast">
         <p>Coming soon!</p>
+        <iframe width="100%" height="600" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/290948600&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
       </article>
     );
   }
