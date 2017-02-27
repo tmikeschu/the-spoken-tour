ccddff9c79b260f0c63dd09f42709a1db91af2f2
Mike Schutte <tmikeschutte@gmail.com>
2017-02-27 08:40:12 -0700

    Add temporary view for podcast

---
+++ src/components/Podcast.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../stylesheets/App.css';
+
+export default class Podcast extends Component {
+  render() {
+    return (
+      <article className="podcast">
+        <p>Coming soon!</p>
+      </article>
+    );
+  }
+}
--- src/router.js
+++ src/router.js
@@ -9,6 +9,7 @@ import About from './components/About.jsx';
 import Instagram from './components/Instagram.jsx';
 import EmbeddedMap from './components/EmbeddedMap.jsx';
 import Contact from './components/Contact.jsx';
+import Podcast from './components/Podcast.jsx';
 
 // Styles
 import './stylesheets/App.css';
@@ -22,6 +23,7 @@ const routes = (
       <Route path="instagram" component={Instagram} />
       <Route path="map" component={EmbeddedMap} />
       <Route path="contact" component={Contact} />
+      <Route path="podcast" component={Podcast} />
     </Route>
   </Router>
 );
