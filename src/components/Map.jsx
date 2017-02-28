336dec3f27ac2e6ebf72db7a79a0f3633e7635a1
Mike Schutte <tmikeschutte@gmail.com>
2017-02-28 09:15:36 -0700

    Restire map and container

---
+++ src/components/Map.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps';
+
+export default class Map extends Component {
+  render() {
+    const mapContainer = <div style={{ height: "90%", width: "75%" }} />
+    const markers = this.props.markers.map((pin, i) => {
+      const marker = {
+        position: {
+          lat: pin.location.lat,
+          lng: pin.location.lng
+        }
+      }
+      return <Marker key={i} {...marker} />
+    })
+
+    const denverRoute = this.props.denverRoute.map((pin) => {
+      return {
+        lat: parseFloat(pin.location.lat),
+        lng: parseFloat(pin.location.lng)
+      }
+    })
+    const polyOptions = {
+      // geodsic: true,
+      strokeColor: "#A00",
+    }
+    const polyline = <Polyline path={denverRoute} options={polyOptions} />
+
+    return(
+      <GoogleMapLoader
+        containerElement={ mapContainer }
+        googleMapElement={
+          <GoogleMap
+            defaultZoom={this.props.zoom}
+            defaultCenter={this.props.center}
+            options={{
+              streetViewControl: false,
+              myTypeControl: false,
+            }}>
+            { markers }
+          </GoogleMap>
+      } />
+    );
+  }
+}
---
+++ src/components/MapContainer.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+
+import Map from './Map';
+
+export default class MapContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      denverRoute: [],
+      carslbadRoute: [],
+      costaRicaRoute: [],
+      darienGapRoute: [],
+    }
+  }
+
+  componentDidMount() {
+    var self = this;
+    $.ajax({
+      url: "http://spoken-api.herokuapp.com/api/v1/route_pins/denver_to_carlsbad?api_key="+process.env.REACT_APP_RAILS_KEY,
+      method: "GET",
+    }).done(function(response) {
+      self.setState({
+        denverRoute: response
+      })
+    }).fail(function(error) {
+      console.error("No");
+    });
+  }
+
+
+  render() {
+    const fullHeightAndWidth = { width: '100vw', height: '100vh' };
+    const mapCenter = {
+      lat: -1.6246706849414423,
+      lng: -85.56376812500002
+    };
+    const mapZoom = 3;
+
+    const markers = [
+      {
+        location: {
+          lat: 39.7392,
+          lng: -104.9903
+        }
+      }
+    ]
+
+    return (
+      <div style={fullHeightAndWidth}>
+        <Map zoom={mapZoom} center={mapCenter} markers={markers} denverRoute={this.state.denverRoute}/>
+      </div>
+    )
+  }
+}
