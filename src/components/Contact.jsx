670f5026773ec0f2370d4273209e5a730881368c
Mike Schutte <tmikeschutte@gmail.com>
2017-03-01 19:03:50 -0700

    Update contact styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,8 +7,10 @@ export default class Contact extends Component {
     return (
       <article className="contact">
         <h3>We would love to hear from you!</h3>
-        <p>Email us at <a href="mailto:thespokentour@gmail.com">thespokentour@gmail.com</a></p>
-        <p>Follow us on Instagram <a href="https://www.instagram.com/thespokentour/">@thespokentour</a></p>
+        <section>
+          <p>Email us at <a href="mailto:thespokentour@gmail.com">thespokentour@gmail.com</a></p>
+          <p>Follow us on Instagram <a href="https://www.instagram.com/thespokentour/">@thespokentour</a></p>
+        </section>
         <ContactForm />
       </article>
     );
