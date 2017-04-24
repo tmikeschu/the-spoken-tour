a32ff7338fa98b05bcd103f7538e9f49b930f7c2
Mike Schutte <tmikeschutte@gmail.com>
2017-04-24 15:01:45 -0600

    Add fields to suggestion pin form
    Refactor component

--- src/App/Map/SuggestionForm/SuggestionForm.jsx
+++ src/App/Map/SuggestionForm/SuggestionForm.jsx
@@ -7,6 +7,8 @@ export default class SuggestionForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      name: '',
+      email: '',
       label: '',
       description: '',
       category: '',
@@ -40,6 +42,8 @@ export default class SuggestionForm extends Component {
           lat: this.props.suggestionPin.lat,
           lng: this.props.suggestionPin.lng,
           message: this.state.message,
+          name: this.state.name,
+          email: this.state.email,
         }
       },
       dataType: "json"
@@ -70,26 +74,54 @@ export default class SuggestionForm extends Component {
   }
 
   render() {
+    const form = (
+      <form onSubmit={this.handleSubmit}>
+        <input required type="text" name="name" value={this.state.name}
+          onChange={this.handleChange} placeholder="Name" />
+
+        <input required type="email" name="email" value={this.state.email}
+          onChange={this.handleChange} placeholder="Email (our eyes only!)" />
+
+        <input required type="text" name="label" value={this.state.label}
+          onChange={this.handleChange} placeholder="Label" />
+
+        <textarea required name="description" value={this.state.description} 
+          onChange={this.handleChange} placeholder="Description" />
+
+        <select required name="category" value={this.state.category} 
+          onChange={this.handleChange} >
+          <option value="">--Category--</option>
+          <option value="0">Place to stay</option>
+          <option value="1">Cool spot</option>
+          <option value="2">Avoid this place</option>
+          <option value="3">Bike shop</option>
+          <option value="4">Other</option>
+        </select>
+
+        <textarea name="message" value={this.state.message} 
+          onChange={this.handleChange} placeholder="Message (optional)" />
+
+        <p style={{
+            display: this.props.suggestionPin.lat === undefined ? "block" : "none"
+          }} 
+          value="Drop It!"
+        >
+          Click the map to drop a pin!
+        </p>
+
+        <input type={
+            this.props.suggestionPin.lat === undefined ? "hidden" : "submit"
+          } 
+          value="Drop It!" />
+
+      </form>
+    );
     return (
       <article className="suggestion-form"
         style={{display: this.props.tabIndex === 0 ? 'none' : 'block'}}
       >
         <h4>Drop a Pin</h4>
-        <form onSubmit={this.handleSubmit}>
-          <input required type="text" name="label" value={this.state.label} onChange={this.handleChange} placeholder="Label" />
-          <textarea required name="description" value={this.state.description} onChange={this.handleChange} placeholder="Description"/>
-          <select required name="category" value={this.state.category} onChange={this.handleChange} >
-            <option value="">--Category--</option>
-            <option value="0">Place to stay</option>
-            <option value="1">Cool spot</option>
-            <option value="2">Avoid this place</option>
-            <option value="3">Bike shop</option>
-            <option value="4">Other</option>
-          </select>
-          <textarea name="message" value={this.state.message} onChange={this.handleChange} placeholder="Optional Message (only seen by us)"/>
-          <p style={{display: this.props.suggestionPin.lat === undefined ? "block" : "none"}} value="Drop It!">Click the map to drop a pin!</p>
-          <input type={this.props.suggestionPin.lat === undefined ? "hidden" : "submit"} value="Drop It!" />
-        </form>
+        { form }
         <Notification
           isActive={this.state.suggestionSent}
           message={this.state.formNotification}
