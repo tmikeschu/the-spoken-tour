407d175ad96a81ba1bb556344ad160e7985b1238
Mike Schutte <tmikeschutte@gmail.com>
2017-04-25 19:32:58 -0600

    Add date to current location in legend
    Restyle legend

--- src/App/Map/Legend/Legend.jsx
+++ src/App/Map/Legend/Legend.jsx
@@ -8,8 +8,14 @@ export default class Legend extends Component {
         <h4>Legend</h4>
         <ul>
           <li style={{display: this.props.tabIndex === 1 ? 'none' : 'block'}}><span style={{color: "#1267FF"}}>◉</span> → Planned Route</li>
-          <li style={{display: this.props.tabIndex === 0 ? 'none' : 'block'}}><span><img src="http://maps.google.com/mapfiles/ms/icons/cycling.png" alt=""/></span> → That's Us!</li>
-          <li style={{display: this.props.tabIndex === 0 ? 'none' : 'block'}}><span style={{color: "#FB7064"}}>◉</span> → Suggestions</li>
+          <li style={{display: this.props.tabIndex === 0 ? 'none' : 'block'}}>
+            <span>
+              <img src="http://maps.google.com/mapfiles/ms/icons/cycling.png" alt="cyclist"/>
+            </span> → That's Us! ( as of {this.props.date} )
+          </li>
+          <li style={{display: this.props.tabIndex === 0 ? 'none' : 'block'}}>
+            <span>◉</span> → Suggestions
+          </li>
         </ul>
       </article>
     );
