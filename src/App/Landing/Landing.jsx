e586b1714f23c360a6fa692159a1b93d3bfdc495
Mike Schutte <tmikeschutte@gmail.com>
2017-04-27 17:20:25 -0600

    Add third video to landing

--- src/App/Landing/Landing.jsx
+++ src/App/Landing/Landing.jsx
@@ -27,6 +27,10 @@ export default class Landing extends Component {
             width="640" height="360" frameborder="0" 
             webkitallowfullscreen mozallowfullscreen allowfullscreen>
           </iframe>
+          <iframe src="https://player.vimeo.com/video/211432545?byline=0&portrait=0" 
+            width="640" height="360" frameborder="0" 
+            webkitallowfullscreen mozallowfullscreen allowfullscreen>
+          </iframe>
         </article>
       </section>
     )
