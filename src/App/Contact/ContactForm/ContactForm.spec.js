9865d91d81162248595718557bac90f50a45edef
Mike Schutte <tmikeschutte@gmail.com>
2017-05-16 10:24:25 -0600

    Add tests for contact form methods
    * Update react shallow dependency
    * Update react prop types dependency

--- src/App/Contact/ContactForm/ContactForm.spec.js
+++ src/App/Contact/ContactForm/ContactForm.spec.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { shallow } from "enzyme"
+import { shallow, mount } from "enzyme"
 import ContactForm from "./ContactForm"
 
 describe("<ContactForm />", () => {
@@ -14,6 +14,44 @@ describe("<ContactForm />", () => {
     expect(contactForm.find("form input[name='_gotcha']").length).toEqual(1)
     expect(contactForm.find("form input[type='submit']").length).toEqual(1)
     expect(contactForm.find("article p").length).toEqual(3)
+  })
+
+  describe("#handleSubmit", () => {
+    it("is called on submit", () => {
+      const restore = ContactForm.prototype.handleSubmit
+      const mock = ContactForm.prototype.handleSubmit = jest.fn()
+      const contactForm = mount(<ContactForm />)
+      contactForm.find(".contact-form form").simulate("submit")
+      expect(mock).toHaveBeenCalled()
+      
+      ContactForm.prototype.handleSubmit = restore
+    })
+  })
+
+  describe("#handleChange", () => {
+    it("is called on a key change", () => {
+      const restore = ContactForm.prototype.handleChange
+      const mock = ContactForm.prototype.handleChange = jest.fn()
+      const contactForm = mount(<ContactForm />)
+      contactForm.find(".contact-form input[name='name']").simulate("change")
+      expect(mock).toHaveBeenCalledTimes(1)
+      contactForm.find(".contact-form input[name='email']").simulate("change")
+      expect(mock).toHaveBeenCalledTimes(2)
+      
+      ContactForm.prototype.handleChange = restore
+    })
+
+    it("updaates state values", () => {
+      const contactForm = mount(<ContactForm />)
+      const nameField = contactForm.find(".contact-form input[name='name']")
+
+      expect(nameField.node.value).toEqual('')
+
+      nameField.node.value = "a"
+      nameField.simulate("change", nameField)
+
+      expect(nameField.node.value).toEqual('a')
+    })
   })
 })
 
