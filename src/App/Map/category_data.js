977b8f95f12ad90b344ad9068105fd019ea21ad4
Mike Schutte <tmikeschutte@gmail.com>
2017-05-22 09:37:15 -0600

    Add https to img links

--- src/App/Map/category_data.js
+++ src/App/Map/category_data.js
@@ -1,11 +1,11 @@
 const categoryIcons = {
-  stay: "http://maps.google.com/mapfiles/ms/icons/blue.png",
-  checkout: "http://maps.google.com/mapfiles/ms/icons/green.png",
-  avoid: "http://maps.google.com/mapfiles/ms/icons/red.png",
-  bike_shop: "http://maps.google.com/mapfiles/ms/icons/mechanic.png",
-  other: "http://maps.google.com/mapfiles/ms/icons/purple.png",
-  endOfDay: "http://maps.google.com/mapfiles/ms/icons/flag.png",
-  cycling: "http://maps.google.com/mapfiles/ms/icons/cycling.png",
+  stay: "https://maps.google.com/mapfiles/ms/icons/blue.png",
+  checkout: "https://maps.google.com/mapfiles/ms/icons/green.png",
+  avoid: "https://maps.google.com/mapfiles/ms/icons/red.png",
+  bike_shop: "https://maps.google.com/mapfiles/ms/icons/mechanic.png",
+  other: "https://maps.google.com/mapfiles/ms/icons/purple.png",
+  endOfDay: "https://maps.google.com/mapfiles/ms/icons/flag.png",
+  cycling: "https://maps.google.com/mapfiles/ms/icons/cycling.png",
 }
 
 const categoryLabels = {
