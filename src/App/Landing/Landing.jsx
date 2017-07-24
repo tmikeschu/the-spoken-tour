c1deab9f2fe1e2b2c3e05a6ddbf7115f25ebb5c8
Mike Schutte <tmikeschutte@gmail.com>
2017-07-23 23:08:48 -0600

    Add link and vid to landing

--- src/App/Landing/Landing.jsx
+++ src/App/Landing/Landing.jsx
@@ -15,17 +15,24 @@ const Landing = () => (
     </article>
 
     <article className="video">
-      <iframe title="Episode 1" src="https://player.vimeo.com/video/211431209?byline=0&portrait=0" 
-        width="640" height="360" frameBorder="0" 
+      <a href="http://www.5280.com/2017/05/du-alumni-to-bike-from-denver-to-patagonia/" target="_blank" rel="noopener noreferrer">
+        Recently featured in Denver's 5280!
+      </a>
+      <iframe title="Episode 1" src="https://player.vimeo.com/video/211431209?byline=0&portrait=0"
+        width="640" height="360" frameBorder="0"
         webkitallowfullscreen mozallowfullscreen allowFullScreen>
       </iframe>
       <iframe title="Episode 2" src="https://player.vimeo.com/video/207747360?byline=0&portrait=0"
-        width="640" height="360" frameBorder="0" 
+        width="640" height="360" frameBorder="0"
         webkitallowfullscreen mozallowfullscreen allowFullScreen>
       </iframe>
-      <iframe title="Episode 3" src="https://player.vimeo.com/video/211432545?byline=0&portrait=0" 
-        width="640" height="360" frameBorder="0" 
+      <iframe title="Episode 3" src="https://player.vimeo.com/video/211432545?byline=0&portrait=0"
+        width="640" height="360" frameBorder="0"
         webkitallowfullscreen mozallowfullscreen allowfullscreen>
+      </iframe>
+      <iframe title="Episode 4" src="https://player.vimeo.com/video/215738304?byline=0&portrait=0"
+        width="640" height="360" frameBorder="0"
+        webkitallowfullscreen mozallowfullscreen allowFullScreen>
       </iframe>
     </article>
   </section>
