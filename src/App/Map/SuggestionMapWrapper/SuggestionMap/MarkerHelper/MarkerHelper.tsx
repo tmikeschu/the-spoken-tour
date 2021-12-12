d4e3636630ed7152f86763062f362facb41c413b
Mike Schutte <tmikeschutte@gmail.com>
2021-12-12 11:20:06 -0500

    fmt

--- src/App/Map/SuggestionMapWrapper/SuggestionMap/MarkerHelper/MarkerHelper.tsx
+++ src/App/Map/SuggestionMapWrapper/SuggestionMap/MarkerHelper/MarkerHelper.tsx
@@ -1,43 +1,43 @@
-import React from "react";
-import { Marker } from "react-google-maps";
-import { Category, categoryIcons } from "../../../category_data";
+import React from "react"
+import { Marker } from "react-google-maps"
+import { Category, categoryIcons } from "../../../category_data"
 
 interface LatLngable {
-  lat: number;
-  lng: number;
+  lat: number
+  lng: number
 }
 
 interface Locationable {
-  location: LatLngable;
-  id: number;
-  category: Category;
+  location: LatLngable
+  id: number
+  category: Category
 }
 
 interface Positionable {
-  position: LatLngable;
+  position: LatLngable
 }
 
 interface Labelable {
-  text: string;
-  fontSize: string;
+  text: string
+  fontSize: string
 }
 
 const makeLabel = (text: string, fontSize: string = "32px"): Labelable => ({
   text,
-  fontSize
-});
+  fontSize,
+})
 
 export const coordinatesFor = (point: Locationable): Positionable => {
   return {
     position: {
       lat: point.location?.lat ?? 0,
-      lng: point.location?.lng ?? 0
-    }
-  };
-};
+      lng: point.location?.lng ?? 0,
+    },
+  }
+}
 
 export const currentLocationPin = (
-  currentLocation: Locationable
+  currentLocation: Locationable,
 ): React.ReactElement => (
   <Marker
     {...coordinatesFor(currentLocation)}
@@ -46,14 +46,14 @@ export const currentLocationPin = (
     label={makeLabel(categoryIcons.cycling, "48px")}
     options={{ clickable: true }}
   />
-);
+)
 
 export const suggestionMarkers = (
   suggestions: Locationable[],
-  handler: Function
+  handler: Function,
 ): React.ReactElement[] =>
   suggestions.map((suggestion, i) => {
-    const marker: Positionable = coordinatesFor(suggestion);
+    const marker: Positionable = coordinatesFor(suggestion)
 
     return (
       <Marker
@@ -63,20 +63,20 @@ export const suggestionMarkers = (
         {...marker}
         onClick={props => handler(props)}
       />
-    );
-  });
+    )
+  })
 
 export const lineCoordinates = (points: Locationable[]): LatLngable[] =>
   points.map(point => ({
     lat: point.location.lat,
-    lng: point.location.lng
-  }));
+    lng: point.location.lng,
+  }))
 
 export const endsOfDayMarkers = (
-  actualPath: Locationable[]
+  actualPath: Locationable[],
 ): React.ReactElement[] =>
   actualPath.slice(0, actualPath.length - 1).map((point, i) => {
-    const marker = coordinatesFor(point);
+    const marker = coordinatesFor(point)
 
     return (
       <Marker
@@ -86,11 +86,11 @@ export const endsOfDayMarkers = (
         // fill="transparent"
         {...marker}
       />
-    );
-  });
+    )
+  })
 
 export const suggestion = (
-  suggestionPin: LatLngable
+  suggestionPin: LatLngable,
 ): React.ReactElement | null =>
   suggestionPin.lat !== undefined ? (
     <Marker
@@ -99,4 +99,4 @@ export const suggestion = (
       icon="none"
       label={makeLabel("📍")}
     />
-  ) : null;
+  ) : null
